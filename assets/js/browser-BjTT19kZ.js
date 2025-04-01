2b886971751af2910e52f972f63359375b032fda
zerkan34 <nicolasd34@gmail.com>
2025-04-01 21:58:29 +0000

    Deploying to gh-pages from @ zerkan34/OYSTERCULT@7dfecca49931466b04d59fca8e7de43df5a3cda6 🚀

---
+++ assets/js/browser-BjTT19kZ.js
@@ -0,0 +1,2 @@
+import{g as e}from"./vendor-Eefq5Uh8.js";var o=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")};const r=e(o),s=Object.freeze(Object.defineProperty({__proto__:null,default:r},Symbol.toStringTag,{value:"Module"}));export{s as b};
+//# sourceMappingURL=browser-BjTT19kZ.js.map
